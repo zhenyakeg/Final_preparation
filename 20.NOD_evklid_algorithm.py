1e062a71d4234a2587b8946c0ae7a7d648d4f19d
Eugene Kegeles <zhenyakeg@gmail.com>
2016-12-12 22:36:56 +0300

    Done few tasks

---
+++ 20.NOD_evklid_algorithm.py
@@ -0,0 +1,21 @@
+def evklid_recursion(a, b):
+    if b == 0:
+        return a
+    return evklid_recursion(b, a%b)
+'''Если два числа делятся на третье число, то разность этих чисел тоже делится на это число,
+будем вычитаать из первого второе максимальное количество рааз и брать то, что осталось, в итоге одно из чисел
+дойдет до нуля и мы получим в качестве второго НОД'''
+def evklid_cycle_clever(a,b):
+    while b > 0:
+        a, b = b, a%b
+    return a
+def evklid_cycle_stupid (a,b):
+    if a == b:
+        return a
+    while a!=b:
+        if a > b:
+            a = a-b
+        else:
+            b = b-a
+    return b
+print(evklid_recursion(435348,8000))
