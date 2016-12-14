aa5767df60b10c7d4cbd2ac22c114c91f9ddc93f
Eugene Kegeles <zhenyakeg@gmail.com>
2016-12-14 12:52:06 +0300

    Done few tasks

--- 29.largest_arising_subsequence.py
+++ 29.largest_arising_subsequence.py
@@ -1,11 +1,10 @@
 def LAS(A):
-    #FIXME #Doesn't work, need changes
     n = len(A)
     l = [0] * n
     for i in range(n):
         for j in range(i):
             if l[j] > l[i] and A[j] < A[i]:
                 l[i] = l[j]
-            l[i] += 1
+        l[i] += 1
     return max(l)
-print(LAS([4, 2, 1, 3, 5,0]))
+print(LAS([1, 3, 4, 5, 7, 1, 9]))
