2d103cedc2cfd34c5a00b8a7caad1529b62657ef
Eugene Kegeles <zhenyakeg@gmail.com>
2017-05-16 01:07:16 +0300

    beginning preparartion for 2nd term exam

---
+++ 2nd term/_24_levenshtein.py
@@ -0,0 +1,13 @@
+s1 = input()
+s2 = input()
+lev_dist = [[0 for i in range(len(s1) + 1)] for j in range(len(s2) + 1)]
+for i in range(len(s2) + 1):
+    lev_dist[i][0] = i
+for j in range(len(s1) + 1):
+    lev_dist[0][j] = j
+for i in range(1, len(s2) + 1):
+    for j in range(1, len(s1) + 1):
+        lev_dist[i][j] = min(lev_dist[i][j - 1],
+                             lev_dist[i - 1][j],
+                             lev_dist[i-1][j-1]) + (0 if s2[i-1] == s1[j - 1] else 1)
+print(lev_dist[-1][-1])
