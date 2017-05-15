4e76d951894347c50985e2b56d64ccd296852bf0
Eugene Kegeles <zhenyakeg@gmail.com>
2017-05-15 14:25:37 +0300

    beginning preparartion for 2nd term exam

---
+++ 2nd term/2_list_edges_to_lists_complementations_w_o.py
@@ -0,0 +1,10 @@
+def create_graph_from_lists_to_complementation_lists():
+    n, m = map(int, input().split())
+    graph = {}
+    for j in range(m):
+        a, b, c = map(int, input().split())
+        graph[a] = graph.get(a, []) + [(b, c)]
+    return graph
+G = create_graph_from_lists_to_complementation_lists()
+for node in G:
+    print(node,':',' '.join(map(str, G[node])))
