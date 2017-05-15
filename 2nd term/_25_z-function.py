5017e69345accf02226e34df9f7ef37733cfd881
Eugene Kegeles <zhenyakeg@gmail.com>
2017-05-16 02:12:40 +0300

    beginning preparartion for 2nd term exam

--- 2nd term/_25_z-function.py
+++ 2nd term/_25_z-function.py
@@ -4,6 +4,7 @@ def z_function(s):
     z = [0] * len(s)
     for i in range(1, len(s)):
         x = (min(z[i - l], r - i + 1) if i <= r else 0)
+        # x = z[i - l] if i <= r else 0
         while i + x < len(s) and s[x] == s[i + x]:
             x += 1
         z[i] = x
@@ -12,3 +13,7 @@ def z_function(s):
     return z
 
 print(' '.join(map(str, z_function(s))))
+
+'abaabahhhhhabaabl - пример с выходом за рамку'
+'0 0 1 3 0 1 0 0 0 0 0 5 0 1 3 0 1'
+'0 0 1 3 0 1 0 0 0 0 0 5 0 1 2 0 0'
